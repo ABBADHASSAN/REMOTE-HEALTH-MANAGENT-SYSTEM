d6bf775261992de764cf02af23ba5a4e9e29c0f7
MUHAMMAD ABBAD UL HASSAN <ibadhassan40@gmail.com>
2025-05-06 16:19:39 +0500

    Update RPMSConsoleMain.java

--- model/RPMSConsoleMain.java
+++ model/RPMSConsoleMain.java
@@ -20,58 +20,148 @@ public class RPMSConsoleMain {
             }
         };
 
-        NotificationService notificationService = new NotificationService(notifier);
-        ReminderService reminderService = new ReminderService(notifier);
-        PanicButton panicButton = new PanicButton(notificationService);
-        EmergencyAlert emergencyAlert = new EmergencyAlert();
 
-        while (true) {
-            System.out.println("\n=== Remote Patient Monitoring System ===");
+        System.out.println("Welcome to the Remote Patient Monitoring System!");
+        System.out.println("Please enter your details to get started.");
+        System.out.print("Enter your user type (lower-case) : ");
+        String userType = scanner.nextLine();
+
+        int choice ;
+        if (userType.equals("admin")) {
+            do {
+            System.out.println("Welcome Admin!");
             System.out.println("1. Add Doctor");
             System.out.println("2. Add Patient");
-            System.out.println("3. Request Appointment");
-            System.out.println("4. Approve Appointment");
-            System.out.println("5. Trigger Panic Button");
-            System.out.println("6. Upload Vital & Check Alert");
-            System.out.println("7. Send Appointment Reminder");
-            System.out.println("8. View Appointments");
-            System.out.println("9. View Registered Doctors");
-            System.out.println("10. View Registered Patients");
-            System.out.println("11. View Vitals from File");
-            System.out.println("12. View Appointments from File");
-            System.out.println("13. Exit");
+            System.out.println("3. View Registered Doctors");
+            System.out.println("4. View Registered Patients");
+            System.out.println("5. View Appointments from File");
+            System.out.println("6. Exit");
+            System.out.print("Choose option: ");
+             choice = scanner.nextInt();
+            scanner.nextLine(); // consume newline
 
+
+                switch (choice) {
+                    case 1 -> addDoctor();
+                    case 2 -> addPatient();
+                    case 3 -> {
+                        System.out.println("\n--- Registered Doctors ---");
+                        try (Scanner fileScanner = new Scanner(new java.io.File("doctors.txt"))) {
+                            while (fileScanner.hasNextLine()) {
+                                String line = fileScanner.nextLine();
+                                System.out.println(line);
+                            }
+                        } catch (IOException e) {
+                            System.out.println("Error reading doctors.txt: " + e.getMessage());
+                        }
+                    }
+                    case 4 -> {
+                        System.out.println("\n--- Registered Patients ---");
+                        try (Scanner fileScanner = new Scanner(new java.io.File("patients.txt"))) {
+                            while (fileScanner.hasNextLine()) {
+                                String line = fileScanner.nextLine();
+                                System.out.println(line);
+                            }
+                        } catch (IOException e) {
+                            System.out.println("Error reading patients.txt: " + e.getMessage());
+                        }
+                    }
+                    case 5 -> AppointmentManager.viewAppointments();
+                    default -> System.out.println("Invalid option!");
+                }
+                System.out.print("Choose option: ");
+                choice = scanner.nextInt();
+                scanner.nextLine(); // consume newline
+            }  while (choice != 6);
+            System.out.println("Exiting system. Goodbye!");
+
+        }
+
+
+        else if (userType.equals("doctor")) {
+            do {
+            System.out.println("Welcome Doctor!");
+            System.out.println("1. Approve Appointment");
+            System.out.println("2. Send Appointment Reminder");
+            System.out.println("3. View Appointments");
+            System.out.println("4. View  Patients");
+            System.out.println("5. Exit");
+            System.out.print("Choose option: ");
+             choice = scanner.nextInt();
+            scanner.nextLine(); // consume newline
+
+
+                switch (choice) {
+                    case 1 -> approveAppointment();
+                    case 2 -> {
+                        System.out.print("Enter patient name: ");
+                        String patient = scanner.nextLine();
+                        System.out.print("Enter appointment time: ");
+                        String time = scanner.nextLine();
+                        ReminderService reminderService = new ReminderService(notifier);
+                        reminderService.sendAppointmentReminder(patient, time);
+                    }
+                    case 3 -> AppointmentManager.viewAppointments();
+                    case 4 -> {
+                        System.out.println("\n--- Registered Patients ---");
+                        try (Scanner fileScanner = new Scanner(new java.io.File("patients.txt"))) {
+                            while (fileScanner.hasNextLine()) {
+                                String line = fileScanner.nextLine();
+                                System.out.println(line);
+                            }
+                        } catch (IOException e) {
+                            System.out.println("Error reading patients.txt: " + e.getMessage());
+                        }
+                    }
+                    default -> System.out.println("Invalid option!");
+                }
+                System.out.print("Choose option: ");
+                choice = scanner.nextInt();
+                scanner.nextLine(); // consume newline
+            }
+            while (choice != 5) ;
+            System.out.println("Exiting system. Goodbye!");
+        }
+
+        // patient user
+        else if (userType.equals("patient")) {
+        do {
+            System.out.println("Welcome Patient!");
+            System.out.println("1. Request Appointment");
+            System.out.println("2. Trigger Panic Button");
+            System.out.println("3. Upload Vital & Check Alert");
+            System.out.println("4. Exit");
             System.out.print("Choose option: ");
 
-            int choice = scanner.nextInt();
-            scanner.nextLine(); // consume newline
-            switch (choice) {
-                case 1 -> addDoctor();
-                case 2 -> addPatient();
-                case 3 -> requestAppointment();
-                case 4 -> approveAppointment();
-                case 5 -> {
-                    System.out.print("Enter patient name: ");
-                    String name = scanner.nextLine();
-                    panicButton.triggerAlert(name);
-                }
-                case 6 -> {
+            NotificationService notificationService = new NotificationService(notifier);
+            ReminderService reminderService = new ReminderService(notifier);
+            PanicButton panicButton = new PanicButton(notificationService);
+            EmergencyAlert emergencyAlert = new EmergencyAlert();
 
-                        Date currentDate = new Date();
-                        System.out.println("Current Date: " + currentDate);
+             choice = scanner.nextInt();
+            scanner.nextLine(); // consume newline
+
+
+                switch (choice) {
+                    case 1 -> requestAppointment();
+                    case 2 -> {
+                        System.out.print("Enter patient name: ");
+                        String name = scanner.nextLine();
+                        panicButton.triggerAlert(name);
+                    }
+                    case 3 -> {
                         System.out.print("Enter patient name: ");
                         String name = scanner.nextLine();
                         System.out.print("Enter heart rate: ");
-                        double heartRate = scanner.nextInt();
+                        double heartRate = scanner.nextDouble();
                         System.out.print("Enter blood pressure: ");
-                        double bloodPressure = scanner.nextInt();
+                        double bloodPressure = scanner.nextDouble();
                         System.out.print("Enter oxygen level: ");
-                        double oxygenLevel = scanner.nextInt();
+                        double oxygenLevel = scanner.nextDouble();
                         System.out.print("Enter temperature: ");
                         double temperature = scanner.nextDouble();
-                        scanner.nextLine(); // consume newline
                         Patient patient = new Patient(name, " ", ""); // Dummy patient object
-                        VitalSign vitalSign = new VitalSign(name,heartRate, bloodPressure, oxygenLevel, temperature,currentDate);
+                        VitalSign vitalSign = new VitalSign(name,heartRate, bloodPressure, oxygenLevel, temperature,new Date());
                         String line =  "name : " + name + "  Heart Rate : " + heartRate + " BP : " + bloodPressure + "Oxygen Level : " + oxygenLevel + "  Temprature : " + temperature;
 
                         try {
@@ -84,77 +174,25 @@ public class RPMSConsoleMain {
                         } catch (IOException ex) {
                             System.out.println("Error writing to file: " + ex.getMessage());
                         }
-
-
-                }
-
-
-                case 7 -> {
-                    System.out.print("Enter patient name: ");
-                    String patient = scanner.nextLine();
-                    System.out.print("Enter appointment time: ");
-                    String time = scanner.nextLine();
-                    reminderService.sendAppointmentReminder(patient, time);
-                }
-                case 8 -> AppointmentManager.viewAppointments();
-
-                case 9 -> {
-                    System.out.println("\n--- Registered Doctors ---");
-                    try (Scanner fileScanner = new Scanner(new java.io.File("doctors.txt"))) {
-                        while (fileScanner.hasNextLine()) {
-                            String line = fileScanner.nextLine();
-                            System.out.println(line);
-                        }
-                    } catch (IOException e) {
-                        System.out.println("Error reading doctors.txt: " + e.getMessage());
                     }
+                    default -> System.out.println("Invalid option!");
                 }
-
-                case 10 -> {
-                    System.out.println("\n--- Registered Patients ---");
-                    try (Scanner fileScanner = new Scanner(new java.io.File("patients.txt"))) {
-                        while (fileScanner.hasNextLine()) {
-                            String line = fileScanner.nextLine();
-                            System.out.println(line);
-                        }
-                    } catch (IOException e) {
-                        System.out.println("Error reading patients.txt: " + e.getMessage());
-                    }
-                }
-
-                case 11 -> {
-                    System.out.println("\n--- Patient Vitals ---");
-                    try (Scanner fileScanner = new Scanner(new java.io.File("vitals.txt"))) {
-                        while (fileScanner.hasNextLine()) {
-                            String line = fileScanner.nextLine();
-                            System.out.println(line);
-                        }
-                    } catch (IOException e) {
-                        System.out.println("Error reading vitals.txt: " + e.getMessage());
-                    }
-                }
-
-                case 12 -> {
-                    System.out.println("\n--- Appointment Requests ---");
-                    try (Scanner fileScanner = new Scanner(new java.io.File("appointments.txt"))) {
-                        while (fileScanner.hasNextLine()) {
-                            String line = fileScanner.nextLine();
-                            System.out.println(line);
-                        }
-                    } catch (IOException e) {
-                        System.out.println("Error reading appointments.txt: " + e.getMessage());
-                    }
-                }
-
-                case 13 -> {
-                    System.out.println("Exiting system. Goodbye!");
-                    return;
-                }
-
-                default -> System.out.println("Invalid option!");
+                System.out.print("Choose option: ");
+                choice = scanner.nextInt();
+                scanner.nextLine(); // consume newline
             }
+            while (choice != 4);
         }
+
+        else {
+            System.out.println("Invalid user type. Exiting.");
+            return;
+        }
+
     }
+
+
+
 
     private static void addDoctor() {
 
