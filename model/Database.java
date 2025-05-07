f69343ea0e5747cc36acc8d5210d82a4911ed17f
MUHAMMAD ABBAD UL HASSAN <ibadhassan40@gmail.com>
2025-05-07 10:57:03 +0500

    Delete model directory

--- model/Database.java
+++
@@ -1,4 +0,0 @@
-package com.example.rpms.model;
-
-public class Database {
-}
--- model/ReportGenerator.java
+++
@@ -1,8 +0,0 @@
-package com.example.rpms.model;
-
-public class ReportGenerator {
-    public void generatePatientReport(Patient patient) {
-        System.out.println("Generating report for: " + patient.getName());
-        // Later: Export as PDF or text file
-    }
-}
