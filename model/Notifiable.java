4a121266d7c74647844570f8774f0bd9bce84b7e
MUHAMMAD ABBAD UL HASSAN <ibadhassan40@gmail.com>
2025-05-09 15:00:03 +0500

    Delete model directory

--- model/Notifiable.java
+++
@@ -1,8 +0,0 @@
-package com.example.rpms.model;
-
-public interface Notifiable {
-
-    void sendNotification(String message, String recipient);
-    void notify(String message, String recipient);
-
-}
